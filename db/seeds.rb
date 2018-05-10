5d560e6b83e93bfd007097e6bac21b68a7cd5afb
Romain Grossard <rgrossard@gmail.com>
2018-05-10 17:37:57 +0200

    mister cocktail Back check

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+  Ingredient.create!(name: "Milk")
+  Ingredient.create!(name: "Beer")
+  Ingredient.create!(name: "Vodka")
+  Ingredient.create!(name: "Mayonnaise")
+  Ingredient.create!(name: "Jägermeister")
+  Ingredient.create!(name: "Lime")
+  Ingredient.create!(name: "Bailey's")
+  Ingredient.create!(name: "Mint Cream")
+  Ingredient.create!(name: "Goldshlagger")
+  Ingredient.create!(name: "Rhum Coco")
+  Ingredient.create!(name: "Whiskey")
+  Ingredient.create!(name: "Tequila")
+  Ingredient.create!(name: "Tomato Juice")
+  Ingredient.create!(name: "Lemon Juice")
+  Ingredient.create!(name: "Bourbon")
+  Ingredient.create!(name: "Irish Whisky")
+  Ingredient.create!(name: "Scotish Whisky")
+  Ingredient.create!(name: "Tennessee Whisky")
+  Ingredient.create!(name: "Cottage Cheese")
+  Ingredient.create!(name: "Tabasco")
+  Ingredient.create!(name: "Worceistershire Sauce")
+  Ingredient.create!(name: "Salt")
+  Ingredient.create!(name: "Raw Egg Yolk")
+  Ingredient.create!(name: "Canned Tuna Juice")
+
+puts 'Finished!'
